0cb68c67adb0c3e24572da8a0ce4ca049a5c4eac
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2024-05-13 09:54:54 +0200

    uz to valí

--- IT3B-Chat.Server/MainWindow.xaml.cs
+++ IT3B-Chat.Server/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Fleck;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,10 +18,13 @@ namespace IT3B_Chat.Server
  public partial class MainWindow : Window
  {
         private WebSocketServer server;
+        private List<IWebSocketConnection> allSockets = new List<IWebSocketConnection>();
+
 
         public MainWindow()
         {
             InitializeComponent();
+            FleckLog.Level = LogLevel.Debug;
         }
         private void BtnStartServer_Click(object sender, RoutedEventArgs e)
         {
@@ -34,12 +38,41 @@ namespace IT3B_Chat.Server
 
         private void StartServer()
         {
-            // Váš kód pro inicializaci a spuštění WebSocket serveru
+            server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Dispatcher.Invoke(() => listBoxLog.Items.Add("Client Connected!"));
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Dispatcher.Invoke(() => listBoxLog.Items.Add("Client Disconnected!"));
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Dispatcher.Invoke(() => listBoxLog.Items.Add("Received: " + message));
+                    foreach (var s in allSockets)
+                    {
+                        s.Send("Echo: " + message);
+                    }
+                };
+            });
+
+            listBoxLog.Items.Add("Server started at ws://0.0.0.0:8181");
         }
+
 
         private void StopServer()
         {
-            // Váš kód pro zastavení WebSocket serveru
+            foreach (var socket in allSockets.ToList())
+            {
+                socket.Close();
+            }
+            server.Dispose();
+            listBoxLog.Items.Add("Server stopped.");
         }
     }
 }
